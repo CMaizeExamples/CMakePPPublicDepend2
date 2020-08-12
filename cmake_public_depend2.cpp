3483dda7ff31f0c865ac9df95c2ea11d579913e9
aafinger <aafinger@iastate.edu>
2020-08-12 14:12:12 -0500

    depend2 lib and cmakelists.txt

---
+++ cmake_public_depend2.cpp
@@ -0,0 +1,7 @@
+#include "cmake_public.hpp"
+#include "cmake_public_depend.hpp"
+#include "cmake_public_depend2.hpp"
+
+int call_cmake_public_depend2(){
+  return call_cmake_public_depend()+2;
+}
---
+++ cmake_public_depend2.hpp
@@ -0,0 +1,6 @@
+#ifndef CMAKE_PUBLIC_DEPEND2_HPP
+#define CMAKE_PUBLIC_DEPEND2_HPP
+
+int call_cmake_public_depend2();
+
+#endif
